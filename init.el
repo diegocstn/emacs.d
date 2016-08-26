eae24f0634e9fa7b586196f2ae9f904fc007e126
Diego Costantino <diegocstn@gmail.com>
2016-08-26 23:45:04 +0200

    custom stuff

--- init.el
+++ init.el
@@ -52,17 +52,7 @@
 
 ;; theme
 (load-theme 'monokai t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("557c283f4f9d461f897b8cac5329f1f39fac785aa684b78949ff329c33f947ec" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;; custom file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
