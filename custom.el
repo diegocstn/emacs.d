72667aa884646c8ac48391a603b526aaa55413dc
Diego Costantino <diegocstn@gmail.com>
2022-08-22 10:02:52 -0700

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (magit utop tuareg editorconfig monokai-theme js2-mode haskell-mode flycheck))))
+    '(magit utop editorconfig monokai-theme haskell-mode flycheck)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
