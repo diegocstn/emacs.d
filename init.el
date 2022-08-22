c9dcf3c59c881cc05f95cc08c6b55f8b5b0353d8
Diego Costantino <diegocstn@gmail.com>
2022-08-22 09:49:01 -0700

    Update init.el

--- init.el
+++ init.el
@@ -28,10 +28,10 @@
 (setq ring-bell-function 'ignore)
 
 ;; remove scrollbar and menu bar
-(if (display-graphic-p)
-    (progn
-      (tool-bar-mode -1)
-      (scroll-bar-mode -1)))
+;;(if (display-graphic-p)
+;;    (progn
+;;      (tool-bar-mode -1)
+;;      (scroll-bar-mode -1)))
 
 ;; ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
